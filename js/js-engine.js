680937cee132a6616ddf70f605a6c8ed322a6573
Maciej Smolinski <msblaze89@gmail.com>
2009-12-27 19:21:05 +0100

    Add class to all inputs

--- js/js-engine.js
+++ js/js-engine.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
-  // put all your jQuery code here
+  // Add class to all inputs (input's type attribute value)
+  $('input').each(function() {$(this).addClass(this.type)});
   
+  // put all your jQuery code here
 });
