b26a3d12ab97414a0525b8281dbe2c753660cc10
Maciej Smolinski <msblaze89@gmail.com>
2009-12-27 20:11:55 +0100

    adding js improvements for better css support in older browsers

--- js/js-engine.js
+++ js/js-engine.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
-  // Add class to all inputs (input's type attribute value)
+  if($.browser.msie)
+  {
+    // Add class [dashed underlines] to all abbr,acronym and dfn tags with title attribute specified
+    $('abbr[title],acronym[title],dfn[title]').addClass("attr_title");
+  }
+  
+  // Add class to all inputs (class = input's `type` attribute value) 
   $('input').each(function() {$(this).addClass(this.type)});
-
+  
   // put all your jQuery code here
 
 });
