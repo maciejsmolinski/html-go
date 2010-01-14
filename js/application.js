7b8a892d3328a931ec674a0492fcf54158530047
Maciej Smolinski <msblaze89@gmail.com>
2010-01-15 00:46:00 +0100

    `jQuery` replaced with `javascript` in a comment

--- js/application.js
+++ js/application.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
     // Add class [dashed underlines] to all abbr,acronym and dfn tags with title attribute specified
     $('abbr[title],acronym[title],dfn[title]').addClass("attr_title");
   }
-  
+
   // Add class to all inputs (class = input's `type` attribute value) 
   $('input').each(function() {$(this).addClass(this.type)});
-  
-  // put all your jQuery code here
+
+  // put all your javascript code here
 
 });
