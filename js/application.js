c06ebd36e7c0d289125109d9b4b4e2eba4d1fef3
Maciej Smolinski <msblaze89@gmail.com>
2010-07-10 18:26:53 +0200

    JS updated to version 0.9

--- js/application.js
+++ js/application.js
@@ -1,14 +1,30 @@
-$(document).ready(function() {
+$(function(){
 
   if($.browser.msie)
   {
-    // Add class [dashed underlines] to all abbr,acronym and dfn tags with title attribute specified
-    $('abbr[title],acronym[title],dfn[title]').addClass("attr_title");
+    // Colorize odd rows
+    $('.colorize_rows > .row:odd').css({'background':'#eee'});
+
+    // Sometimes clearfix does not help
+    $('div:last-child').after('<div class="clear" />');
+
+    // Higlight selected field on focus by changing border-color
+    var ie_fields = $('input[type=text],input[type=password],textarea,select')
+    ie_fields.focus(function() { $(this).css({'border':'1px solid #9C9C9C'}) });
+    ie_fields.blur(function() { $(this).css({'border':'1px solid #ddd'}) });
   }
 
-  // Add class to all inputs (class = input's `type` attribute value) 
-  $('input').each(function() {$(this).addClass(this.type)});
+  // Add class to all inputs (ie. input[type=text] = input.text) - for IE (< 7) purposes mainly, if needed
+  $('input').each(function() { $(this).addClass(this.type); });
 
-  // put all your javascript code here
+  // Add `clear:both` to any element after last `.col` (which is floated left) mainly for orphaned `.col`s
+  $('.col:last + *').css({'clear':'both'});
+
+  // Highlight selected field's row on focus by changing row's background-color
+  var fields = $('.colorize_rows input, .colorize_rows textarea, .colorize_rows select')
+  fields.focus(function() { $(this).parents('.row').addClass('focus'); });
+  fields.blur(function() { $(this).parents('.row').removeClass('focus'); });
+
+  // Put all your javascript code here ...
 
 });
