8ecc7d4f563957026f8fc17f93ad2e141605ec06
Maciej Smolinski <msblaze89@gmail.com>
2011-03-20 03:49:30 +0100

    Changing (missing-)placeholder beahaviour detection; Adding some sample commented out code snippets

--- js/application.js
+++ js/application.js
@@ -1,37 +1,49 @@
+  $(function(){
 
-head.ready("jquery", function() {
+    $('body').addClass('js');
 
-    $(function(){
+    if(!"placeholder" in document.createElement("input"))
+    {
+      // Simulate placeholder functionality
+      $('input[placeholder]').each(function() {
+        $(this).attr('value') == '' ?  $(this).attr('value',$(this).attr('placeholder')) : true;
+      })
+      $('input[placeholder]').focus(function() {
+        $(this).attr('value') == $(this).attr('placeholder') ?  $(this).attr('value','') : true;
+      })
+      $('input[placeholder]').blur(function() {
+        $(this).attr('value') == '' ?  $(this).attr('value',$(this).attr('placeholder')) : true;
+      })
+    }
 
-      $('body').addClass('js');
 
-      if(!$.browser.webkit && !$.browser.opera)
-      {
-        // Simulate placeholder functionality
-        $('input[placeholder]').each(function() {
-          $(this).attr('value') == '' ?  $(this).attr('value',$(this).attr('placeholder')) : true;
-        })
-        $('input[placeholder]').focus(function() {
-          $(this).attr('value') == $(this).attr('placeholder') ?  $(this).attr('value','') : true;
-        })
-        $('input[placeholder]').blur(function() {
-          $(this).attr('value') == '' ?  $(this).attr('value',$(this).attr('placeholder')) : true;
-        })
-      }
+    // all javascript code here
 
-      if($.browser.msie)
-      {
-        // IE6-notice [PL]:
-        // $('body').append('<div id="ie6-notice"><p id="ie6-text">Korzystasz ze starszej wersji przeglądarki Internet Explorer, przez co nasza strona może nie działać poprawnie. Proponujemy aktualizację oprogramowania do jednej z następujących przeglądarek: <a href="http://www.mozilla.com/firefox/">Firefox</a>, <a href="http://www.apple.com/safari/">Safari</a>, <a href="http://www.opera.com/">Opera</a> lub <a href="http://www.microsoft.com/windows/internet-explorer/">Internet Explorer 8</a><br /><br />Bardzo dziękujemy za wizytę!</p></div>');
-        // IE6-notice [EN]:
-        $('body').append('<div id="ie6-notice"><p id="ie6-text">You appear to be browsing this site using Internet Explorer 6. This browser is now out of date. For safer, more reliable browsing it is recommended that you upgrade your browser to to <a href="http://www.mozilla.com/firefox/">Firefox</a>, <a href="http://www.apple.com/safari/">Safari</a>, <a href="http://www.opera.com/">Opera</a>, or <a href="http://www.microsoft.com/windows/internet-explorer/">Internet Explorer 8</a>');
+    // $('#map').gMap({
+    //   markers: [{ address: "Poznań, Tęczowa 19a",
+    //               html: "<strong>MSM EVENTS Sp. z o.o.</strong><br />ul. Tęczowa 19a, 60-275 Poznań",
+    //               popup: true }],
+    //   zoom: 15
+    // });
 
-      }
+    // $("#carousel").carousel({
+    //   dispItems: 1,
+    //   pagination: true,
+    //   nextBtn: '',
+    //   prevBtn: '',
+    //   loop: true,
+    //   autoSlide: true,
+    //   autoSlideInterval: 5000,
+    //   effect: 'fade'
+    // });
 
-    });
 
-});
+    if($.browser.msie)
+    {
+      // IE6-notice [PL]:
+      // $('body').append('<div id="ie6-notice"><p id="ie6-text">Korzystasz ze starszej wersji przeglądarki Internet Explorer, przez co nasza strona może nie działać poprawnie. Proponujemy aktualizację oprogramowania do jednej z następujących przeglądarek: <a href="http://www.mozilla.com/firefox/">Firefox</a>, <a href="http://www.apple.com/safari/">Safari</a>, <a href="http://www.opera.com/">Opera</a> lub <a href="http://www.microsoft.com/windows/internet-explorer/">Internet Explorer 8</a><br /><br />Bardzo dziękujemy za wizytę!</p></div>');
+      // IE6-notice [EN]:
+      $('body').append('<div id="ie6-notice"><p id="ie6-text">You appear to be browsing this site using Internet Explorer 6. This browser is now out of date. For safer, more reliable browsing it is recommended that you upgrade your browser to to <a href="http://www.mozilla.com/firefox/">Firefox</a>, <a href="http://www.apple.com/safari/">Safari</a>, <a href="http://www.opera.com/">Opera</a>, or <a href="http://www.microsoft.com/windows/internet-explorer/">Internet Explorer 8</a>');
+    }
 
-head.js(
-   {jquery: "http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"}
-);
+  });
